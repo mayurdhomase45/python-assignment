8ee72127cda6bf9b120a16188830f7539cd3f99f
Mayur <mayurdhomse75@gmail.com>
2026-06-29 20:14:29 +0530

    Added operators assignment

---
+++ operators_types.py
@@ -0,0 +1,40 @@
+# Arithmetic Operators
+
+a = 25
+b = 4
+
+print("Arithmetic Operators")
+print("----------------------")
+print("Addition :", a + b)
+print("Subtraction :", a - b)
+print("Multiplication :", a * b)
+print("Division :", a / b)
+print("Floor Division :", a // b)
+print("Modulus :", a % b)
+print("Exponent :", a ** b)
+
+print()
+
+# Comparison Operators
+
+print("Comparison Operators")
+print("----------------------")
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+print()
+
+# Logical Operators
+
+x = 15
+y = 8
+
+print("Logical Operators")
+print("----------------------")
+print(x > 10 and y < 10)
+print(x < 10 or y < 10)
+print(not(x == y))
